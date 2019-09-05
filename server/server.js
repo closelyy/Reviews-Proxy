41ee1504bc97f59671673446af4b4a6bb7d78d65
Dan Fox <fox.j.dan@gmail.com>
2019-09-04 19:58:49 -0700

    adjusted to combine modules on a single page

--- server/server.js
+++ server/server.js
@@ -2,33 +2,21 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const request = require('request');
+const Axios = require('axios');
 
 const app = express();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
-app.get('/:params', (req, res) => { 
-  console.log(`Received ${req.method} request from ${req.url}`);
-  console.log('extra parameters:      ', req.params);
-
-  const apiUrl = 'http://localhost:3000' + req.url;
-  console.log(apiUrl);
-  request(apiUrl, (error, response, body) => { 
-    if (!error && response.statusCode === 200) { 
-      res.send(body); 
-    } 
-   }); 
-});
-
 app.get('/api/reviews/:params', (req, res) => { 
   console.log(`Received ${req.method} request from ${req.url}`);
   console.log('extra parameters:      ', req.params);
 
-  const apiUrl = 'http://localhost:3000' + req.url;
+  const apiUrl = 'http://localhost:3001' + req.url;
   console.log(apiUrl);
   request(apiUrl, (error, response, body) => { 
     if (!error && response.statusCode === 200) { 
@@ -39,7 +27,7 @@ app.get('/api/reviews/:params', (req, res) => {
 
 app.post('/api/reviews/vote', (req, res) => { 
   console.log(`3001 Server:       Received ${req.method} request from ${req.url}`);
-  const apiUrl = 'http://localhost:3000' + req.url;
+  const apiUrl = 'http://localhost:3001' + req.url;
   console.log(req.body);
   request({ 
     url: apiUrl,
@@ -56,8 +44,26 @@ app.post('/api/reviews/vote', (req, res) => {
 
 app.get('/Pictures/:filename', (req, res) => { 
   console.log(`Received ${req.method} request from ${req.url}`);
-  const apiUrl = 'http://localhost:3000' + req.url;
-  request.get(apiUrl).pipe(res); 
+  const apiUrl = 'http://localhost:3001' + req.url;
+  request.get(apiUrl).pipe(res);
+});
+
+app.get('/api/reservations/', (req, res) => {
+  console.log(`${req.method} request from ${req.url}`);
+  Axios.get('http://localhost:3003/api/reservations/')
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(200);
+    });
+});
+
+app.get('/api/businesses/:businessId/photos', (req, res) => { 
+  console.log(`Received ${req.method} request from ${req.url}`);
+  const apiUrl = 'http://localhost:3002' + req.url;
+  request.get(apiUrl).pipe(res);
 });
 
 app.listen(port, () => {
